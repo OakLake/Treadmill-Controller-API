44c75dd05c9e78c191b0ce1dfcf07ae022fcac4b
Sammy Hasan <sammy.nm.hasan@gmail.com>
2025-02-28 18:19:04 +0000

    Add telemetry widgets

--- src/tui/app.py
+++ src/tui/app.py
@@ -4,10 +4,30 @@ from bleak import BleakClient
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalScroll
-from textual.widgets import Button, Digits, Footer, Header
+from textual.widgets import Button, Digits, Footer, Header, Label
 
 from src.treadmill.controller import TreadmillController
 from src.treadmill.secret import TREADMILL_ADDR
+
+
+class SpeedDisplay(Digits):
+    """A widget to display the treadmill speed."""
+
+
+class DurationDisplay(Digits):
+    """A widget to display the treadmill duration."""
+
+
+class CaloriesDisplay(Digits):
+    """A widget to display the treadmill calories."""
+
+
+class DistanceDisplay(Digits):
+    """A widget to display the treadmill distance."""
+
+
+class StepsDisplay(Digits):
+    """A widget to display the treadmill steps."""
 
 
 class TreadMillApp(App):
@@ -15,17 +35,31 @@ class TreadMillApp(App):
 
     BINDINGS = []
 
-    def __init__(self, treadmill_controller: TreadmillController):
+    def __init__(
+        self, treadmill_controller: TreadmillController, telemetry_queue: asyncio.Queue
+    ):
         super().__init__()
+        self.q = telemetry_queue
         self.controller = treadmill_controller
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield Button("Start", id="start", variant="success")
-        yield Button("Stop", id="stop", variant="error")
-        yield Button("+", id="inc_speed", variant="success")
-        yield Button("-", id="dec_speed", variant="error")
+        yield VerticalScroll(
+            HorizontalGroup(
+                Button("Stop", id="stop", variant="error"),
+                Button("Start", id="start", variant="success"),
+            ),
+            HorizontalGroup(
+                Button("-", id="dec_speed", variant="error"),
+                Button("+", id="inc_speed", variant="success"),
+            ),
+            HorizontalGroup(Label("Duratinon"), DurationDisplay("00:00:00")),
+            HorizontalGroup(Label("Speed"), SpeedDisplay("Speed")),
+            HorizontalGroup(Label("Calories"), CaloriesDisplay("000")),
+            HorizontalGroup(Label("Distance"), DistanceDisplay("0000")),
+            HorizontalGroup(Label("Steps"), StepsDisplay("0000")),
+        )
 
     @on(Button.Pressed, "#start")
     async def start_treadmill(self):
@@ -52,11 +86,19 @@ async def run():
 
     async with BleakClient(TREADMILL_ADDR) as client:
         print("Connected")
+
         treadmill_controller = TreadmillController(
             client, control_point_uuid, data_point_uuid, telemetry_queue
         )
-        app = TreadMillApp(treadmill_controller)
+        task = asyncio.create_task(treadmill_controller.subscribe())
+
+        app = TreadMillApp(treadmill_controller, telemetry_queue)
         await app.run_async()
+
+    task.cancel()
+    await task
+    # treadmill_controller.stop_event.is_set()
+    # await client.stop_notify(data_point_uuid)
 
 
 if __name__ == "__main__":
